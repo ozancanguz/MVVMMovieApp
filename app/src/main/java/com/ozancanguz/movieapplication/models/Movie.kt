1a1478d0f62974bdb0ff930df3bf79f1e187d507
Ozan <ozancangu24@gmail.com>
2022-12-06 22:44:20 +0300

    -For room we modified model class

--- app/src/main/java/com/ozancanguz/movieapplication/models/Movie.kt
+++ app/src/main/java/com/ozancanguz/movieapplication/models/Movie.kt
@@ -2,17 +2,31 @@ package com.ozancanguz.movieapplication.models
 
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
-@Parcelize
+@Entity()
 data class Movie(
+
+    @ColumnInfo(name="db_category")
     @SerializedName("category")
     val category: String,
+
+    @ColumnInfo(name="db_desc")
     @SerializedName("desc")
     val desc: String,
+
+    @ColumnInfo(name="db_imageUrl")
     @SerializedName("imageUrl")
     val imageUrl: String,
+
+    @ColumnInfo(name="db_name")
     @SerializedName("name")
     val name: String
-):Parcelable
+){
+    @PrimaryKey(autoGenerate = true)
+    var uuid:Int=0
+}
