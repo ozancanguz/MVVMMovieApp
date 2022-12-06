01d70dd80a02985f5c31ff5b7f97d2802e053697
Ozan <ozancangu24@gmail.com>
2022-12-06 23:25:19 +0300

    Revert "-For room we modified model class"
    This reverts commit 1a1478d0

--- app/src/main/java/com/ozancanguz/movieapplication/models/Movie.kt
+++ app/src/main/java/com/ozancanguz/movieapplication/models/Movie.kt
@@ -2,31 +2,17 @@ package com.ozancanguz.movieapplication.models
 
 
 import android.os.Parcelable
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
-@Entity()
+@Parcelize
 data class Movie(
-
-    @ColumnInfo(name="db_category")
     @SerializedName("category")
     val category: String,
-
-    @ColumnInfo(name="db_desc")
     @SerializedName("desc")
     val desc: String,
-
-    @ColumnInfo(name="db_imageUrl")
     @SerializedName("imageUrl")
     val imageUrl: String,
-
-    @ColumnInfo(name="db_name")
     @SerializedName("name")
     val name: String
-){
-    @PrimaryKey(autoGenerate = true)
-    var uuid:Int=0
-}
+):Parcelable
