f49600ec938e01e995faf63fef0895f68ecfed6c
Ozan <ozancangu24@gmail.com>
2022-12-06 13:10:48 +0300

    -In MovieViewModel.kt we request the data

---
+++ app/src/main/java/com/ozancanguz/movieapplication/viewmodels/MovieViewModel.kt
@@ -0,0 +1,41 @@
+package com.ozancanguz.movieapplication.viewmodels
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.ozancanguz.movieapplication.data.repository.Repository
+import com.ozancanguz.movieapplication.models.Movie
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class MovieViewModel(private val repository: Repository,application: Application):AndroidViewModel(application) {
+
+
+    private val movieList=MutableLiveData<List<Movie>>()
+
+   var job: Job?=null
+
+
+    fun requestApiData(){
+
+        job= CoroutineScope(Dispatchers.IO).launch {
+           val response=repository.remote.getMovies()
+            if(response.isSuccessful){
+                movieList.postValue(response.body())
+            }
+            else{
+                Log.d("error","no data")
+            }
+
+        }
+
+    }
+
+
+}
