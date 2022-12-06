d2ea0c0c85811b586ad93ba330585cc0064e7e4f
Ozan <ozancangu24@gmail.com>
2022-12-06 12:37:08 +0300

    -navigation created

---
+++ app/src/main/java/com/ozancanguz/movieapplication/ui/fragments/movieListFragment/MovieListFragment.kt
@@ -0,0 +1,23 @@
+package com.ozancanguz.movieapplication.ui.fragments.movieListFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ozancanguz.movieapplication.R
+
+
+class MovieListFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_list, container, false)
+    }
+
+
+}
